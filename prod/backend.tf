42974dc5b47de3e1ee0cd44fc2a80d58bf0ca077
matheus victorio <127612031+matheusvictorio@users.noreply.github.com>
2026-09-12 02:53:31 -0300

    infra: configure Azure Blob Terraform backend

---
+++ prod/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-totvs-tfstate"
+    storage_account_name = "sttotvstf123456"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+    use_azuread_auth     = true
+  }
+}
+
