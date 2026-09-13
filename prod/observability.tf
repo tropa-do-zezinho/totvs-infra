997a5621fc4780bfa5b380ce3daccc8b7006531a
matheus victorio <127612031+matheusvictorio@users.noreply.github.com>
2026-09-13 16:54:42 -0300

    style: format observability configuration

--- prod/observability.tf
+++ prod/observability.tf
@@ -8,7 +8,7 @@ resource "azurerm_log_analytics_workspace" "prod" {
   sku                 = "PerGB2018"
 
   retention_in_days = 30
-  daily_quota_gb   = 0.1
+  daily_quota_gb    = 0.1
 
   tags = {
     project     = "totvs"
